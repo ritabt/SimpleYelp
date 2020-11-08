b1372f196bd067d61e8bcdbe9b3547a15bb8e5c9
​Rita Tlemcani <ritabt@stanford.edu>
2020-11-08 15:57:29 -0800

    working version

--- app/src/main/java/com/rita/simpleyelp/RestaurantAdapter.kt
+++ app/src/main/java/com/rita/simpleyelp/RestaurantAdapter.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_restaurant.view.*
 
 class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaurants>) :
@@ -24,6 +28,15 @@ class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaura
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         fun bind(restaurant: YelpRestaurants) {
             itemView.tvName.text = restaurant.name
+            itemView.ratingBar.rating = restaurant.rating.toFloat()
+            itemView.tvNumReviews.text = "${restaurant.numReviews} Reviews"
+            itemView.tvAddress.text = restaurant.location.address
+            itemView.tvCategory.text = restaurant.categories[0].title
+            itemView.tvDistance.text = restaurant.displayDistance()
+            itemView.tvPrice.text = restaurant.price
+            Glide.with(context).load(restaurant.imageUrl).apply(RequestOptions().transforms(
+               CenterCrop(), RoundedCorners(20)
+            )).into(itemView.imageView)
         }
 
     }
